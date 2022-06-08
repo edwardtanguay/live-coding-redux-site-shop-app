b7f4407882f4916495deaf777c11b6ea03e427fa
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-08 09:40:26 +0200

    cart is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
-import { PageAbout } from './pages/PageAbout';
+import { PageCart } from './pages/PageCart';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -11,12 +11,12 @@ function App() {
 			<hr />
 			<NavLink to="/welcome">Welcome</NavLink> |{' '}
 			<NavLink to="/books">Books</NavLink> |{' '}
-			<NavLink to="/about">About</NavLink>
+			<NavLink to="/cart">Cart</NavLink>
 			<hr />
 			<Routes>
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/books" element={<PageBooks />} />
-				<Route path="/about" element={<PageAbout />} />
+				<Route path="/cart" element={<PageCart />} />
 				<Route path="/" element={<Navigate to="/welcome" replace />}/>
 			</Routes>
 		</div>
